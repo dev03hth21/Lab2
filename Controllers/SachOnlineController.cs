bdf2eb6a2faf8f4ebddc188be18b4ee9cf5642ae
dev03hth21 <hoangtrunghien2k3@gmail.com>
2023-10-21 17:30:51 +0700

    Lab2

---
+++ Controllers/SachOnlineController.cs
@@ -0,0 +1,44 @@
+﻿using SachOnline.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SachOnline.Controllers
+{
+    public class SachOnlineController : Controller
+    {
+        // GET: SachOnline
+        private string connection;
+        private dbSachOnlineDataContext data;
+
+        public SachOnlineController()
+        {
+            // Khởi tạo chuỗi kết nối
+            connection = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=SachOnline;Integrated Security=True";
+            data = new dbSachOnlineDataContext(connection);
+        }
+        private List<SACH> LaySachMoi(int count) 
+        {
+            return data.SACHes.OrderByDescending(a=> a.NgayCapNhat).Take(count).ToList();
+        }
+        public ActionResult Index()
+        {
+            //lay 6 quyen sach moi
+            var listSachMoi = LaySachMoi(6);
+
+            return View(listSachMoi);
+        }
+        public ActionResult ChuDePartial()
+        {
+            var listChuDe = from cd in data.CHUDEs select cd;
+            return PartialView(listChuDe);
+        }
+        public ActionResult NhaXuatBanPartial() 
+        {
+            var listNhaXuatBan = from cd in data.NHAXUATBANs select cd;
+            return PartialView(listNhaXuatBan);
+        }
+    }
+}
